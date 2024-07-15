fa2b3734ecf0d3cd191479a0d14b65c620c7fb8a
Andrew Contreras <andrewcontreras1215@gmail.com>
2024-07-15 00:03:28 -0700

    Added getCoordinates() method in WeatherService class

--- src/main/java/com/example/demo/services/WeatherService.java
+++ src/main/java/com/example/demo/services/WeatherService.java
@@ -2,20 +2,35 @@ package com.example.demo.services;
 
 import com.example.demo.entities.GeocodingResponse;
 import com.example.demo.entities.WeatherResponse;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import com.example.demo.entities.GeocodingResponse.Location;
+
+import java.util.List;
 
 public class WeatherService {
 
     private RestTemplate restTemplate;
 
     private final String API_KEY = "f30b4a09860080b1fd2050774983619a";
-    private final String BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+    private final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+
+    private final String GEOCODING_URL = "http://api.openweathermap.org/data/2.5/weather";
 
     public WeatherResponse getWeather(String city, String units, String lang) {
         Location location = getCoordinates(city);
 
-        return fetchWeather(city, units, lang);
+        return null; //fetchWeather(city, units, lang);
+    }
+
+    private Location getCoordinates(String city) {
+        String url = String.format("%s?q=%s&appid=%s", GEOCODING_URL, city, API_KEY);
+        ResponseEntity<List<GeocodingResponse.GeocodingResult>> response = restTemplate.exchange(url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<GeocodingResponse.GeocodingResult>>() {});
+
+        return null;
     }
 
 
